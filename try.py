37047b23cc23fc36a9d9140e0b83751e896009e5
Gary Bishop <gary.bishop.unc@gmail.com>
2011-09-17 16:01:42 -0400

    cut stuff I don't need

--- try.py
+++ try.py
@@ -1,17 +1,14 @@
-from hid import hid, hid_enumerate
+import hid
 import time
 
-for i in hid_enumerate():
-    print i
-    
-h = Hid(0x461, 0x20)
+h = hid.device(0x461, 0x20)
 
 h.set_nonblocking(1)
 
-for k in range(100):
+for k in range(10):
     for i in [0, 1]:
         for j in [0, 1]:
-            h.write([0, i, j])
+            h.write([0x80, i, j])
             d = h.read(5)
             if d:
                 print d
