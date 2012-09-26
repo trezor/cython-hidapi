9b96b467a12f7ac5bf1a6c6856202dccdeffd40c
Pieter Rautenbach <parautenbach@gmail.com>
2012-09-26 08:21:06 +0200

    Need to explicitly specify libraries to compile hid.so under Debian 0.1.12 and Ubuntu 12.04. Thanks to Stéfan van der Walt's help on this.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import os
 os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
-os.environ['LDFLAGS'] = "-L/usr/lib/i386-linux-gnu -lusb-1.0 -ludev -lrt"
+os.environ['LDFLAGS'] = "-L/usr/lib/x86_64-linux-gnu -lusb-1.0 -ludev -lrt"
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"])]
+    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"],
+                  libraries=["usb-1.0", "udev", "rt"])]
 )
