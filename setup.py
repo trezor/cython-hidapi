ba65c185a107271e6f5939fe5f787b620efd3945
Pavol Rusnak <stick@gk2.sk>
2013-03-10 13:17:06 +0100

    combine 3 setup ways into a one universal setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,28 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import os
-os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
-os.environ['LDFLAGS'] = "-L/usr/lib/i386-linux-gnu -lusb-1.0 -ludev -lrt"
+import sys
+
+sources = ["hid.pyx"]
+
+if sys.platform.startswith('linux'):
+    os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
+    os.environ['LDFLAGS'] = "-lusb-1.0 -ludev -lrt"
+    sources.append("hid-libusb.c")
+    libs = ["usb-1.0", "udev", "rt"]
+
+if sys.platform.startswith('darwin'):
+    os.environ['CFLAGS'] = "-framework IOKit -framework CoreFoundation"
+    os.environ['LDFLAGS'] = ""
+    sources.append("hid-mac.c")
+    libs = []
+
+if sys.platform.startswith('win'):
+    sources.append("hid-windows.c")
+    libs = ["setupapi"]
 
 setup(
+    name = 'hidapi',
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"])]
+    ext_modules = [Extension("hid",  sources, libraries = libs)]
 )
