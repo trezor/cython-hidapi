49136b72a50e2e07ec2948191860d3cf3a3fc199
slush <info@bitcoin.cz>
2013-03-10 13:06:23 +0100

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+
+# Depends on libusb-1.0-0-dev, libudev-dev
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -7,5 +11,6 @@ os.environ['LDFLAGS'] = "-L/usr/lib/i386-linux-gnu -lusb-1.0 -ludev -lrt"
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"])]
+    ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"],
+                   libraries=["usb-1.0", "udev", "rt"])]
 )
