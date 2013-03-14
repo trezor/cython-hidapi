5750f91fb4bba1a6e492c2d1e27cfc42a960fbfe
slush <info@bitcoin.cz>
2013-03-14 18:16:37 +0100

    Added metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+# dependencies: libusb-1.0-0-dev, libudev-dev
+
+# python setup.py sdist upload
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -8,7 +12,6 @@ import sys
 
 sources = ["hid.pyx"]
 
-# dependencies: libusb-1.0-0-dev, libudev-dev
 if sys.platform.startswith('linux'):
     os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
     os.environ['LDFLAGS'] = "-lusb-1.0 -ludev -lrt"
@@ -27,6 +30,16 @@ if sys.platform.startswith('win'):
 
 setup(
     name = 'hidapi',
+    version = '0.7.0',
+    description = 'A Cython interface to the hidapi from https://github.com/signal11/hidapi',
+    author = 'gbishop',
+    author_email = 'gb@cs.unc.edu',
+    url = 'https://github.com/gbishop/cython-hidapi',
+    classifiers = [
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+    ],
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("hid",  sources, libraries = libs)]
 )
