a9990d1e8196dbef8c84e7a42f84510edeb4ed81
Masaki Muranaka <monaka@monami-ya.jp>
2013-04-17 12:42:18 +0900

    -framework option should be used on the link time.

--- setup-mac.py
+++ setup-mac.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import os
-os.environ['CFLAGS'] = "-framework IOKit -framework CoreFoundation -I"  + os.path.join(os.getcwd(), "hidapi", "hidapi")
-os.environ['LDFLAGS'] = ""
+os.environ['CFLAGS'] = "-I"  + os.path.join(os.getcwd(), "hidapi", "hidapi")
+os.environ['LDFLAGS'] = "-framework IOKit -framework CoreFoundation"
 
 setup(
     cmdclass = {'build_ext': build_ext},
