85ec8894b7391f5f5450da8385c68838dde7893b
Stefan Rupp <struppi@struppi.name>
2013-08-08 16:56:27 +0200

    fixed stuff after merge, so it builds again

--- setup.py
+++ setup.py
@@ -10,15 +10,16 @@ from Cython.Distutils import build_ext
 import os
 import sys
 
-sources = ["hid.pyx", os.path.join(os.getcwd(), "hidapi", "libusb", "hid.c")]]
+sources = ["hid.pyx", os.path.join(os.getcwd(), "hidapi", "libusb", "hid.c")]
 
 if sys.platform.startswith('linux'):
-    sources_raw = ["hid-raw.pyx", "hid-linux-raw.c"] 
-    sources.append("hid-libusb.c")
+    #sources_raw = ["hid-raw.pyx", "hid-linux-raw.c"] 
+    sources_raw = ["hid-raw.pyx",]
+    #sources.append("hid-libusb.c")
     libs = ["usb-1.0", "udev", "rt"]
     libs_raw = ["udev", "rt"]
-    modules = [Extension("hid",  sources, libraries = libs, extra_compile_args=["-I/usr/include/libusb-1.0"])
-            , Extension("hidraw", sources_raw, libraries=libs_raw)]
+    modules = [Extension("hid",  sources, libraries = libs, include_dirs=["/usr/include/libusb-1.0", os.path.join(os.getcwd(), "hidapi", "hidapi")])
+            , Extension("hidraw", sources_raw, libraries=libs_raw, include_dirs=[os.path.join(os.getcwd(), "hidapi", "hidapi")])]
 
 if sys.platform.startswith('darwin'):
     os.environ['CFLAGS'] = "-framework IOKit -framework CoreFoundation"
