f95b1c37fd50330ef0939c1a069c04a813cf179e
Pavol Rusnak <pavol@rusnak.io>
2020-10-28 14:43:51 +0100

    enable python 3.9
    (but not on appveyor which does not support it yet)

--- setup.py
+++ setup.py
@@ -123,6 +123,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     ext_modules = modules,
     setup_requires = ['Cython'],
