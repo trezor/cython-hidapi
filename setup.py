690ed194fc0831d0af51f36980f6d93068e5cd1d
Pavol Rusnak <pavol@rusnak.io>
2022-01-15 20:40:56 +0100

    bump hidapi to 0.11.2

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ if "bsd" in sys.platform:
 
 setup(
     name="hidapi",
-    version="0.11.0.post3",
+    version="0.11.2",
     description="A Cython interface to the hidapi from https://github.com/libusb/hidapi",
     long_description=open("README.rst", "rt").read(),
     author="Pavol Rusnak",
